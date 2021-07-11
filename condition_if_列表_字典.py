d947402826d6f2cd39c53dd177851c34a8c1dead
chengcongming <18829034518@163.com>
2021-07-11 18:34:54 +0800

    字典的学习

--- condition_if_列表_字典.py
+++ condition_if_列表_字典.py
@@ -50,7 +50,6 @@ split_line()
 """
 5-10
 留下的问题:名字需要区分大小写，小写的登录后，大写的不能再进行登录
-
 """
 current_users = ["zhang1", "zhang2", "zhang3", "zhang4", "zhang5"]
 new_users = ["zhao1", "zhang1", "zhao3", "zhang5", "zhao4"]
@@ -59,3 +58,66 @@ for _ in new_users:
         print("name:%s 使用过" % _)
     else:
         print("name:%s 未进行使用" % _)
+split_line()
+
+"""
+以下为字典的学习基础
+6_1
+"""
+persion = {"name":"ccm", "first_name":"cheng", "last_name":"ming", "age":"20", "city":"xian","age":"25", "city":"ankang"}
+print(persion["name"].title())
+print(persion["first_name"])
+print(persion["last_name"])
+print(persion["age"])
+print(persion["city"].title())
+split_line()
+
+"""
+循环遍历
+打印key value
+"""
+for key, value in persion.items():
+    print("\nkey:" + key.title())
+    print("value:" + value.title())
+split_line()
+
+"""
+打印KEY
+"""
+for key in persion.keys():  #等价于 for key in persion:
+    print("key:" + key.title()) #大写
+split_line()
+
+"""
+打印值
+"""
+for value in persion.values():
+    print("values:" + value.title())
+split_line()
+
+"""
+排序打印建
+"""
+for key in sorted(persion.keys()):
+    print("after Sorted key:" + key.title())
+split_line()
+
+"""
+去除字典中的重复的字段
+"""
+for key in set(persion.keys()):
+    print("去重复key:" + key.title())
+split_line()
+"""
+6-5 练习题
+"""
+rivers = {"shanxi":"xian", "hubei":"wuhan", "guangdong":"guanzhou", "zhejiang":"hangzhou"}
+for key,  value in rivers.items():
+    print("The " + key.title() + "provience's city is " + value.title())
+split_line()
+for key in rivers.keys():
+    print("key:" + key.title())
+split_line()
+for value in rivers.values():
+    print("value:" + value.title())
+split_line()
