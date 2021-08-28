27c00fc7c219e2fb391852c83f2f430d21152898
chengcongming <18829034518@163.com>
2021-08-28 17:04:26 +0800

    [增加][静态的HTTP服务器的实现]

---
+++ 004 http/http_server.py
@@ -0,0 +1,96 @@
+import socket
+
+"""
+TCP 的服务端
+1，socket 创建socket
+2.bind 绑定IP和端口
+3.listen 处于监听状态
+4.accept 接进来客户端的连接
+5.recv/send 接受或者发送信息
+6.close 关闭
+"""
+
+def tcp_creat_socket():
+    tcp_ser = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    return tcp_ser
+
+def tcp_bind_listern_info(tcp_socket):
+    SER_IP = "127.0.0.1"
+    # SER_PORT = int(input("服务器绑定的端口:"))
+    SER_PORT = 7080
+    SER_INFO = (SER_IP, SER_PORT)
+    tcp_socket.bind(SER_INFO)
+
+    LISTEN_NUM = 10
+    tcp_socket.listen(LISTEN_NUM)
+    return SER_INFO
+
+def tcp_accept(tcp_socket,ser_info):
+    # accept  1默认会阻塞，2并返回一个新的套接字用于和接进来的客户端进行数据传输，3并记录客户端的信息,为用户进行服务
+    print("服务器的IP：%s,端口:%d,正在等待新的客户端的到来" % (ser_info[0], ser_info[1]))
+    tcp_ser_new_socket, client_addr = tcp_socket.accept()
+    print("客户端已经到来,信息如下:%s" % str(client_addr))
+    return tcp_ser_new_socket
+
+def tcp_recv_message(tcp_socket):
+    # recv  返回值为空时，则客户端调用了close
+    tcp_ser_recv_info = tcp_socket.recv(1024)
+    if "exit" == tcp_ser_recv_info or None == tcp_ser_recv_info:
+        print("recv_info error")
+    # GET / HTTP/1.1
+    # Host: www.baidu.com
+    # Connection: keep-alive
+    # sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"
+    # sec-ch-ua-mobile: ?0
+    print(">"*120)
+    print("服务端接收:%s" % tcp_ser_recv_info.decode("utf-8"))
+
+def tcp_send_message(tcp_socket):
+    # send_info = str(input("输入服务端发送的信息:"))
+
+    header = "HTTP/1.1 200 OK\r\n"  # 相应头部
+    respone = "\r\n"  # 空行
+
+    http_headr = header + respone
+
+    f = open("./pass.html", "rb")  # 二进制文件
+    html_file = f.read()
+    f.close()
+
+    http_body = html_file
+    tcp_socket.send(http_headr.encode("utf-8"))
+    tcp_socket.send(http_body)
+
+    tcp_socket.close()
+
+def tcp_close(tcp_socket):
+    tcp_socket.close()
+
+
+def CC_server():
+    # 创建
+    tcp_ser =tcp_creat_socket()
+
+    # bind and listen
+    ser_info = tcp_bind_listern_info(tcp_socket=tcp_ser)
+
+    while True:  # 循环为多个客户端服务，一直处于监听状态
+        new_socket = tcp_accept(tcp_socket=tcp_ser, ser_info=ser_info)
+
+        while True:  # 循环为同一个用户服务多次
+            # recv  返回值为空时，则客户端调用了close
+            tcp_recv_message(tcp_socket=new_socket)
+
+            # send
+            tcp_send_message(tcp_socket=new_socket)
+            break
+
+        #  close 关闭发送数据的客户端的连接
+        tcp_close(tcp_socket=new_socket)
+
+    # close
+    tcp_close(tcp_socket=tcp_ser)
+
+
+if __name__ == "__main__":
+    CC_server()
---
+++ 004 http/http包.py
@@ -0,0 +1,42 @@
+
+# 应答头
+# HTTP/1.1 200 OK
+# Bdpagetype: 1
+# Bdqid: 0xee5e9efb000153b6
+# Cache-Control: private
+# Connection: keep-alive
+# Content-Encoding: gzip
+# Content-Type: text/html;charset=utf-8
+# Date: Sat, 28 Aug 2021 06:33:15 GMT
+# Expires: Sat, 28 Aug 2021 06:33:12 GMT
+# Server: BWS/1.1
+# Set-Cookie: BDSVRTM=0; path=/
+# Set-Cookie: BD_HOME=1; path=/
+# Set-Cookie: H_PS_PSSID=34434_34378_34497_31254_33848_34092_26350_34390; path=/; domain=.baidu.com
+# Strict-Transport-Security: max-age=172800
+# Traceid: 1630132395046225639417176340829711717302
+# X-Frame-Options: sameorigin
+# X-Ua-Compatible: IE=Edge,chrome=1
+# Transfer-Encoding: chunked
+
+# 应答的数据
+
+
+
+
+# 请求
+# GET / HTTP/1.1
+# Host: www.baidu.com
+# Connection: keep-alive
+# sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"
+# sec-ch-ua-mobile: ?0
+# Upgrade-Insecure-Requests: 1
+# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36
+# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+# Sec-Fetch-Site: none
+# Sec-Fetch-Mode: navigate
+# Sec-Fetch-User: ?1
+# Sec-Fetch-Dest: document
+# Accept-Encoding: gzip, deflate, br
+# Accept-Language: zh-CN,zh;q=0.9
+# Cookie: BIDUPSID=3E1F5D95033C393AA1FB25661068F882; PSTM=1625495117; BAIDUID=3E1F5D95033C393ACC033140812140FC:FG=1; __yjs_duid=1_90bdc02b154560b7e3713c1652d1df9f1625843176548; BAIDUID_BFESS=3E1F5D95033C393ACC033140812140FC:FG=1; BAIDU_WISE_UID=wapp_1629020653894_491; COOKIE_SESSION=10_0_6_9_3_10_0_0_5_8_8_2_720214_0_16_0_1629630427_0_1629630411%7C9%23417292_4_1625912422%7C2; BDORZ=FFFB88E999055A3F8A630C64834BD6D0; ab_sr=1.0.1_YTQ2YzIyNWRkMzI1MDZkODljZGJkZTEwMzIyN2E2ODNkNTE4YzIxZGY2YmQxYTFjODczMzE0NjNlYWJhMDkzODkwNDM0Yjk0YWZjZGRjNmJlNmIxMWM2MmUxZjNmN2E5YWNhYWZiM2I2NzUwZDZjZWZjMWY0NWMxNGRjM2ZkOGEwNGRiMTgxMDE3MmZkMDVmNTc2ODkyYzcyMzFiN2YwNw==; BDRCVFR[86pcH-5hBUC]=I3RFtBO5p3fThNLPWR8mvqV; BD_HOME=1; H_PS_PSSID=31254_26350; BD_UPN=12314753; delPer=0; BD_CK_SAM=1; PSINO=7; H_PS_645EC=8d4etPQMNJRJtQ7ges6EAqwg2jEOydHXe6N4Cwj2QWnUslrGddaIlffh7mehNRtOK946F0Lau8k; BA_HECTOR=2gal0l8k84al8l055e1gijlkn0q; BDSVRTM=307
